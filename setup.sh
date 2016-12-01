f797e11b3434cfd90d4effd00e5fa10069bfb88a
Daniel Sanchez <danielsanchez@nevada.unr.edu>
2016-12-01 00:25:33 -0800

    added urxvt and config

--- setup.sh
+++ setup.sh
@@ -3,5 +3,11 @@
 rm ~/.bashrc
 ln -s ~/dotfiles/bash/.bashrc ~/.bashrc
 
+rm ~/.xinitrc
+ln -s ~/xinitrc ~/.xinitrc
+
+rm ~/.Xresources
+ln -s ~/Xresources ~/.Xresources
+
 rm -r ~/.config/i3
 ln -s ~/dotfiles/i3 ~/.config
