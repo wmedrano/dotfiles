123271af884d115b1fd1170e43177a428a5a16ae
foob <danielsanchez@nevada.unr.edu>
2017-01-18 23:01:47 -0800

    add emacs folder & name its for consistency

--- setup.sh
+++ setup.sh
@@ -13,4 +13,8 @@ rm -r ~/.config/i3
 ln -s ~/dotfiles/i3 ~/.config
 
 rm ~/.xsession
-ln -s ~/dotfiles/.xsession ~/.xsession
+ln -s ~/dotfiles/xsession ~/.xsession
+
+rm -r ~/.emacs.d
+ln -s ~/dotfiles/emacs.d ~/.emacs.d
+#mv ~/emacs.d ~/.emacs.d
