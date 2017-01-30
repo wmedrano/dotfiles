ec3647a44098cc50f96db10534931f3dc66922ad
Will <wmedrano@users.noreply.github.com>
2017-01-29 19:11:34 -0800

    Add melpa
    You need to run `M-x package-refresh-contents`  (M-x is alt-x)
    
    Then you can `M-x package-install` packages from [melpa](https://melpa.org/#/)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,3 +12,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; package management
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+;;; (package-refresh-contents) ;; add melpa packages to M-x package-install
